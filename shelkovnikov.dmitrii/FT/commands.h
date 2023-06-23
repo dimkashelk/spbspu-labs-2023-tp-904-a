54798db9f1a930460207d8a074f636dcc07b5e47
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-06-23 17:37:38 +0300

    add load command

--- shelkovnikov.dmitrii/FT/commands.h
+++ shelkovnikov.dmitrii/FT/commands.h
@@ -1,5 +1,11 @@
 #ifndef FT_COMMANDS_H
 #define FT_COMMANDS_H
+#include <map>
+#include <string>
+using frequency_dict = std::map< std::string, size_t >;
+using text_dict = std::map< std::string, std::string >;
 namespace dimkashelk
-{}
+{
+  void load(text_dict &dict, std::string filename);
+}
 #endif
