c85453621c5e811cd6477cbac72f746fa4043a99
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-06-26 12:16:15 +0300

    add initializeName

--- shelkovnikov.dmitrii/FT/commandcontainer.cpp
+++ shelkovnikov.dmitrii/FT/commandcontainer.cpp
@@ -1,4 +1,14 @@
 #include "commandcontainer.h"
 #include "commands.h"
-dimkashelk::CommandContainer::CommandContainer()
+using con = dimkashelk::CommandContainer;
+con::CommandContainer()
 {}
+std::map< std::string, dimkashelk::func_with_name > con::initializeName()
+{
+  std::map< std::string, func_with_name > res;
+  res["load"] = load;
+  res["analyze"] = analyze;
+  res["delete"] = deleteDict;
+  res["remove"] = remove;
+  return res;
+}
