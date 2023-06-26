dcf38462062e153b5dd11dc9f22e2a2d4c88d407
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-06-26 12:11:15 +0300

    add func_with_1_name_ostream

--- shelkovnikov.dmitrii/FT/commandcontainer.h
+++ shelkovnikov.dmitrii/FT/commandcontainer.h
@@ -8,6 +8,7 @@ namespace dimkashelk
   using func_with_3_name = void(*)(all_data &, c_s, c_s, c_s);
   using func_with_size_t = void(*)(all_data &, c_s, size_t, std::ostream &);
   using func_with_2_name_ostream = void(*)(all_data &, c_s, c_s, std::ostream &);
+  using func_with_1_name_ostream = void(*)(all_data &, c_s, std::ostream &);
   class CommandContainer
   {
   public:
