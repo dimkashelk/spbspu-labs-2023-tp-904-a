63b06be2197bd072cf3a688a65020315879a5614
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-06-26 12:08:28 +0300

    add func_with_3_name

--- shelkovnikov.dmitrii/FT/commandcontainer.h
+++ shelkovnikov.dmitrii/FT/commandcontainer.h
@@ -5,6 +5,7 @@ namespace dimkashelk
 {
   using func_with_name = void(*)(all_data &, c_s);
   using func_with_2_name = void(*)(all_data &, c_s, c_s);
+  using func_with_3_name = void(*)(all_data &, c_s, c_s, c_s);
   class CommandContainer
   {
   public:
