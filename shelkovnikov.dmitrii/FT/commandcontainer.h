c143424c702e55979cf9f1cd3ac606ea8a2ee6ba
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-06-26 12:09:38 +0300

    add func_with_size_t

--- shelkovnikov.dmitrii/FT/commandcontainer.h
+++ shelkovnikov.dmitrii/FT/commandcontainer.h
@@ -6,6 +6,7 @@ namespace dimkashelk
   using func_with_name = void(*)(all_data &, c_s);
   using func_with_2_name = void(*)(all_data &, c_s, c_s);
   using func_with_3_name = void(*)(all_data &, c_s, c_s, c_s);
+  using func_with_size_t = void(*)(all_data &, c_s, size_t, std::ostream &);
   class CommandContainer
   {
   public:
