d830436d025d9047582a80a52301dde9eb9aa698
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-07-02 21:44:32 +0300

    add outInvalidCommand

--- shelkovnikov.dmitrii/FT/main.cpp
+++ shelkovnikov.dmitrii/FT/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include "commandcontainer.h"
+std::ostream &outInvalidCommand(std::ostream &out)
+{
+  return out << "<INVALID COMMAND>";
+}
 int main()
 {
+  dimkashelk::CommandContainer commandContainer(std::cin, std::cout);
+  while (std::cin)
+  {
+    std::string command;
+    std::cin >> command;
+    try
+    {
+      commandContainer.doCommand(command);
+    }
+    catch (const std::logic_error &e)
+    {
+
+    }
+  }
   return 0;
 }
