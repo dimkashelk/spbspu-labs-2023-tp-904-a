985370b20e1dc3d0cc66ffa403e6017193e33ce7
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-07-14 20:12:29 +0300

    update

--- shelkovnikov.dmitrii/FT/commandcontainer.cpp
+++ shelkovnikov.dmitrii/FT/commandcontainer.cpp
@@ -6,58 +6,34 @@ con::CommandContainer(std::istream &in, std::ostream &out):
   in_(in),
   out_(out),
   data_(),
-  dictWithName_(initializeName()),
-  dictWith2Name_(initialize2Name()),
-  dictWith3Name_(initialize3Name()),
-  dictWithSizeT_(initializeSizeT()),
-  dictWith2NameOstream_(initialize2NameOstream()),
-  dictWith1NameOstream_(initialize1NameOstream()),
-  dictWithOstream_(initializeOstream())
+  dictWithIstream_(initializeIstream()),
+  dictWithOstream_(initializeOstream()),
+  dictWithIOstream_(initializeIOstream())
 {}
-std::map< std::string, dimkashelk::func_with_name > con::initializeName()
+std::map< std::string, func_with_istream > con::initializeIstream()
 {
-  std::map< std::string, func_with_name > res;
+  std::map< std::string, func_with_istream > res;
   res["load"] = load;
   res["analyze"] = analyze;
-  res["delete"] = deleteDict;
-  res["remove"] = remove;
-  return res;
-}
-std::map< std::string, dimkashelk::func_with_2_name > con::initialize2Name()
-{
-  std::map< std::string, func_with_2_name > res;
   res["export"] = exportToFile;
   res["exportWithText"] = exportWithText;
   res["deleteWord"] = deleteWord;
-  return res;
-}
-std::map< std::string, dimkashelk::func_with_3_name > con::initialize3Name()
-{
-  std::map< std::string, func_with_3_name > res;
-  res["complement"] = getComplement;
+  res["delete"] = deleteDict;
+  res["remove"] = remove;
   res["intersect"] = getIntersect;
+  res["complement"] = getComplement;
   res["union"] = getUnion;
   return res;
 }
-std::map< std::string, dimkashelk::func_with_size_t > con::initializeSizeT()
+std::map< std::string, func_with_iostream > con::initializeIOstream()
 {
-  std::map< std::string, func_with_size_t > res;
+  std::map< std::string, func_with_iostream > res;
+  res["printWord"] = printWord;
+  res["print"] = print;
   res["top"] = getTop;
   return res;
 }
-std::map< std::string, dimkashelk::func_with_2_name_ostream > con::initialize2NameOstream()
-{
-  std::map< std::string, func_with_2_name_ostream > res;
-  res["printWord"] = printWord;
-  return res;
-}
-std::map< std::string, dimkashelk::func_with_1_name_ostream > con::initialize1NameOstream()
-{
-  std::map< std::string, func_with_1_name_ostream > res;
-  res["print"] = print;
-  return res;
-}
-std::map< std::string, dimkashelk::func_with_ostream > con::initializeOstream()
+std::map< std::string, func_with_ostream > con::initializeOstream()
 {
   std::map< std::string, func_with_ostream > res;
   res["help"] = help;
@@ -67,47 +43,15 @@ void con::doCommand(const std::string &command)
 {
   try
   {
-    auto comm = dictWithName_.at(command);
-    auto f = inputString();
-    comm(data_, f);
+    auto comm = dictWithIstream_.at(command);
+    comm(data_, in_);
   }
   catch (const std::out_of_range &e)
   {}
   try
   {
-    auto comm = dictWith2Name_.at(command);
-    auto p = input2String();
-    comm(data_, p.first, p.second);
-  }
-  catch (const std::out_of_range &e)
-  {}
-  try
-  {
-    auto comm = dictWith3Name_.at(command);
-    auto p = input3String();
-    comm(data_, std::get< 0 >(p), std::get< 1 >(p), std::get< 2 >(p));
-  }
-  catch (const std::out_of_range &e)
-  {}
-  try
-  {
-    auto comm = dictWithSizeT_.at(command);
-    comm(data_, inputString(), inputSizeT(), out_);
-  }
-  catch (const std::out_of_range &e)
-  {}
-  try
-  {
-    auto comm = dictWith2NameOstream_.at(command);
-    auto p = input2String();
-    comm(data_, p.first, p.second, out_);
-  }
-  catch (const std::out_of_range &e)
-  {}
-  try
-  {
-    auto comm = dictWith1NameOstream_.at(command);
-    comm(data_, inputString(), out_);
+    auto comm = dictWithIOstream_.at(command);
+    comm(data_, in_, out_);
   }
   catch (const std::out_of_range &e)
   {}
--- shelkovnikov.dmitrii/FT/commandcontainer.h
+++ shelkovnikov.dmitrii/FT/commandcontainer.h
@@ -3,15 +3,11 @@
 #include <iostream>
 #include "commands.h"
 #include "io.h"
+using func_with_istream = void(*)(all_data &, std::istream &);
+using func_with_ostream = void(*)(std::ostream &);
+using func_with_iostream = void(*)(all_data &, std::istream &, std::ostream &);
 namespace dimkashelk
 {
-  using func_with_name = void(*)(all_data &, c_s);
-  using func_with_2_name = void(*)(all_data &, c_s, c_s);
-  using func_with_3_name = void(*)(all_data &, c_s, c_s, c_s);
-  using func_with_size_t = void(*)(all_data &, c_s, size_t, std::ostream &);
-  using func_with_2_name_ostream = void(*)(all_data &, c_s, c_s, std::ostream &);
-  using func_with_1_name_ostream = void(*)(all_data &, c_s, std::ostream &);
-  using func_with_ostream = void(*)(std::ostream &);
   class CommandContainer
   {
   public:
@@ -21,20 +17,12 @@ namespace dimkashelk
     std::istream &in_;
     std::ostream &out_;
     all_data data_;
-    std::map< std::string, func_with_name > dictWithName_;
-    std::map< std::string, func_with_2_name > dictWith2Name_;
-    std::map< std::string, func_with_3_name > dictWith3Name_;
-    std::map< std::string, func_with_size_t > dictWithSizeT_;
-    std::map< std::string, func_with_2_name_ostream > dictWith2NameOstream_;
-    std::map< std::string, func_with_1_name_ostream > dictWith1NameOstream_;
+    std::map< std::string, func_with_istream > dictWithIstream_;
     std::map< std::string, func_with_ostream > dictWithOstream_;
-    std::map< std::string, func_with_name > initializeName();
-    std::map< std::string, func_with_2_name > initialize2Name();
-    std::map< std::string, func_with_3_name > initialize3Name();
-    std::map< std::string, func_with_size_t > initializeSizeT();
-    std::map< std::string, func_with_2_name_ostream > initialize2NameOstream();
-    std::map< std::string, func_with_1_name_ostream > initialize1NameOstream();
+    std::map< std::string, func_with_iostream > dictWithIOstream_;
+    std::map< std::string, func_with_istream > initializeIstream();
     std::map< std::string, func_with_ostream > initializeOstream();
+    std::map< std::string, func_with_iostream > initializeIOstream();
     std::string inputString();
     std::pair< std::string, std::string > input2String();
     std::tuple< std::string, std::string, std::string > input3String();
