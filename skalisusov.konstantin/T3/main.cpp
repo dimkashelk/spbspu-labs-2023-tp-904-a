e60afad57ba15952f7688023281bdf4bbe286c8c
Kikos1337 <skalisusov@inbox.ru>
2023-08-18 12:42:17 +0300

    creation of temporary main

--- skalisusov.konstantin/T3/main.cpp
+++ skalisusov.konstantin/T3/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include "Polygon.h"
 
 int main()
 {
+  std::vector< skalisusov::Polygon> polygon;
+  constexpr auto max_size = std::numeric_limits< std::streamsize >::max();
+  while(!std::cin.eof())
+  {
+    std::copy(std::istream_iterator< skalisusov::Polygon>(std::cin),
+      std::istream_iterator<skalisusov::Polygon>(),std::back_inserter(polygon));
+    if(!std::cin)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+
   std::cout << "skalisusov.konstantin/T3\n";
   return 0;
 }
