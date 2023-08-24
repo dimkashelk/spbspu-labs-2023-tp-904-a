c86f095b5f0fab81b5faf7be82b4241e749b9f85
Kikos1337 <skalisusov@inbox.ru>
2023-08-25 01:58:46 +0300

    Creation of new functions for commands

--- skalisusov.konstantin/T3/Command.h
+++ skalisusov.konstantin/T3/Command.h
@@ -1,6 +1,7 @@
 #ifndef GITLABPROG_COMMAND_H
 #define GITLABPROG_COMMAND_H
 #include <iostream>
+#include <map>
 #include "Polygon.h"
 namespace skalisusov
 {
@@ -10,12 +11,19 @@ namespace skalisusov
   void areaNumOfVertex(const std::vector< Polygon > &dest,std::size_t vertex ,std::ostream &out);
   void maxArea(const std::vector< Polygon > &dest,std::ostream &out);
   void maxVertex(const std::vector< Polygon > &dest,std::ostream &out);
-  void mixArea(const std::vector< Polygon > &dest,std::ostream &out);
-  void mixVertex(const std::vector< Polygon > &dest,std::ostream &out);
+  void minArea(const std::vector< Polygon > &dest,std::ostream &out);
+  void minVertex(const std::vector< Polygon > &dest,std::ostream &out);
   void countEven(const std::vector< Polygon > &dest, std::ostream &out);
   void countOdd(const std::vector< Polygon > &dest, std::ostream &out);
   void countVertex(const std::vector< Polygon > &dest, std::size_t vertex, std::ostream  &out);
   void rects(const std::vector< Polygon > &dest, std::ostream &out);
+
+  void rmecho(std::vector< Polygon > &dest,std::istream &in, std::ostream &out);
+
+  bool isNumVertex(const Polygon &polygon, std::size_t count);
+  bool isEven(const Polygon &poly);
+  bool isOdd(const Polygon &poly);
+  bool isRects(const Polygon &poly);
 
 }
 #endif //GITLABPROG_COMMAND_H
