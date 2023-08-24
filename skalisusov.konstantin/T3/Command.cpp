ebcac75bdd2190a3c78889a8864bd4910298481a
Kikos1337 <skalisusov@inbox.ru>
2023-08-25 01:59:56 +0300

    realization function for ckeck

--- skalisusov.konstantin/T3/Command.cpp
+++ skalisusov.konstantin/T3/Command.cpp
@@ -1,1 +1,20 @@
 #include "Command.h"
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <cmath>
+
+bool skalisusov::isNumVertex(const skalisusov::Polygon &polygon, std::size_t count)
+{
+  return (polygon.polygon.size() == count);
+}
+bool skalisusov::isEven(const skalisusov::Polygon &poly)
+{
+  return (poly.polygon.size() % 2 == 0);
+}
+bool skalisusov::isOdd(const skalisusov::Polygon &poly)
+{
+  return (poly.polygon.size() % 2 != 0);
+}
