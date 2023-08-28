46b2ba5fc0bcf83c7efebeba6ae162b94152dfdc
Kikos1337 <skalisusov@inbox.ru>
2023-08-28 16:01:11 +0300

    create main.cpp

--- skalisusov.konstantin/T3/main.cpp
+++ skalisusov.konstantin/T3/main.cpp
@@ -1,20 +1,50 @@
 #include <iostream>
 #include <iterator>
 #include <algorithm>
+#include <fstream>
 #include "Polygon.h"
-
-int main()
+#include "Command.h"
+int main(int argc, char ** argv)
 {
-  std::vector< skalisusov::Polygon> polygon;
-  while(!std::cin.eof())
+  using namespace skalisusov;
+  if(argc != 2)
   {
-    std::copy(std::istream_iterator< skalisusov::Polygon>(std::cin),
-      std::istream_iterator<skalisusov::Polygon>(),std::back_inserter(polygon));
-    if(!std::cin)
+    std::cerr << "Not enough CL parameters\n";
+    return 1;
+  }
+  std::ifstream input(argv[1]);
+   if(!input)
+   {
+   std::cerr << "Not open file\n";
+   return 1;
+   }
+
+   auto max = std::numeric_limits< std::streamsize>::max();
+  std::vector< Polygon > polygon;
+  while(!input.eof())
+  {
+    std::copy(std::istream_iterator< Polygon >(input),
+      std::istream_iterator< Polygon >(),std::back_inserter(polygon));
+    if(input.fail())
     {
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      input.clear();
+      input.ignore(max,'\n');
     }
   }
+  input.close();
+  auto mapCommand = command();
+   while(!std::cin.eof())
+   {
+     try
+     {
+       std::string com = listenCommand(std::cin);
+       realizationCommandPolygon(std::cin,std::cout,polygon,mapCommand,com);
+     }
+     catch(std::logic_error &e)
+     {
+       skalisusov::errorMessage(std::cout);
+       std::cin.ignore(max, '\n');
+     }
+   }
   return 0;
 }
