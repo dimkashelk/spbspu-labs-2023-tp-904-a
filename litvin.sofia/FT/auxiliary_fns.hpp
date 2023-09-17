ae4514dd24fb0485ac0388627b4ef6c180d06fe8
Litvin Sofia <sofialitvin823@gmail.com>
2023-09-17 12:12:36 +0300

    added encapsulation

--- litvin.sofia/FT/auxiliary_fns.hpp
+++ litvin.sofia/FT/auxiliary_fns.hpp
@@ -8,20 +8,22 @@ namespace litvin
   command_descriptors createCommandDescriptorsDict();
   void man(const command_descriptors & cmd_desc, std::ostream & out, std::istream & in);
   void help(const command_descriptors & cmd_desc, std::ostream & out, std::istream & in);
-  struct command_dict_t
+  class command_dict_t
   {
+   public:
     command_dict_t();
+    void executeCommand(const std::string & command, dicts_list_t & list, std::ostream & out, std::istream & in) const;
+    void executeCommand(const std::string & command, dicts_list_t & list, const std::string & dict,
+                        std::ostream & out) const;
+    void executeCommand(const std::string & command, const command_descriptors & cmd_desc, std::ostream & out,
+                        std::istream & in) const;
+   private:
     using signature_type_1 = void (*)(dicts_list_t &, std::ostream &, std::istream &);
     using signature_type_2 = void (*)(const command_descriptors &, std::ostream &, std::istream &);
     using signature_type_3 = void (*)(dicts_list_t &, const std::string &, std::ostream &);
     std::map< std::string, signature_type_1 > dict1;
     std::map< std::string, signature_type_2 > dict2;
     std::map< std::string, signature_type_3 > dict3;
-    void executeCommand(const std::string & command, dicts_list_t & list, std::ostream & out, std::istream & in) const;
-    void executeCommand(const std::string & command, dicts_list_t & list, const std::string & dict,
-                        std::ostream & out) const;
-    void executeCommand(const std::string & command, const command_descriptors & cmd_desc, std::ostream & out,
-                        std::istream & in) const;
   };
   std::string inputCommand(std::istream & in);
   void runCommand(const command_dict_t & cmd_d, dicts_list_t & list, const command_descriptors & desc_d,
