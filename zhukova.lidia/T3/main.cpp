6fadf65d540fa14a4e30a93ea81cbf791db55519
honore <lidiya.zhukova.vn@yandex.ru>
2023-09-28 13:15:09 +0300

    process of fixing <invalid command>

--- zhukova.lidia/T3/main.cpp
+++ zhukova.lidia/T3/main.cpp
@@ -42,7 +42,9 @@ int main(int argc, char * argv[])
       break;
     }
     catch (std::logic_error & e){
-      std::cout << e.what();
+      if (!std::cin.eof()) {
+        std::cout << e.what() << "\n";
+      }
       std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
