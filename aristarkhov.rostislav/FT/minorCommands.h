f3af154ec0c872e66bf1245c3509c8da1fd26daf
Twink1eToes <aristarhov.ro@edu.spbstu.ru>
2023-10-24 23:15:27 +0300

    add minorCommand

---
+++ aristarkhov.rostislav/FT/minorCommands.h
@@ -0,0 +1,19 @@
+#ifndef MINORCOMMANDS_H
+#define MINORCOMMANDS_H
+
+#include <iostream>
+#include <map>
+
+namespace aristarkhov
+{
+  using Dictionary = std::map< std::string, size_t >;
+  using dictionaryOfNames = std::map< std::string, Dictionary >;
+
+  bool searchDict(const dictionaryOfNames& dictionaries, const std::string& dict_name);
+  bool searchWrd(const Dictionary& dictionaries, const std::string& word);
+  bool compareFrequency(const std::pair< std::string, size_t >& a, const std::pair< std::string, size_t >& b);
+  void validateDictExists(const dictionaryOfNames& dictionaries, const std::string& dict_name);
+  void printError(std::ostream& out);
+  void skipUntilNewLine(std::istream& in);
+}
+#endif
