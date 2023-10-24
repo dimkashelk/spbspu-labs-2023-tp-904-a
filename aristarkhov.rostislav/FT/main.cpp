bfa6c0bb354b8ab07abee0d04dfed38a07f3c36b
Twink1eToes <aristarhov.ro@edu.spbstu.ru>
2023-10-25 00:26:01 +0300

    realize main

--- aristarkhov.rostislav/FT/main.cpp
+++ aristarkhov.rostislav/FT/main.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <sstream>
 
+#include "commands.h"
 int main()
 {
+  using namespace aristarkhov;
+  dictionaryOfNames dictionaries;
+
+  while (!std::cin.eof())
+  {
+    try
+    {
+      std::string cmd = "";
+      std::cin >> cmd;
+      processCommand(dictionaries, cmd, std::cin, std::cout);
+    }
+    catch (const std::logic_error& e)
+    {
+      std::cout << "Error: " << e.what() << '\n';
+      skipUntilNewLine(std::cin);
+    }
+    catch (const std::runtime_error& e)
+    {
+      std::cout << "Error: " << e.what() << '\n';
+      skipUntilNewLine(std::cin);
+    }
+  }
   return 0;
 }
