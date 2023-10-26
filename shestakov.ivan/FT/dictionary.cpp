0c91edc71e1db6bb57ce23f20c3bb8a076b1f769
Ivan31122003 <Ivan31122003@mail.ru>
2023-10-26 13:21:29 +0300

    delete returnSet

--- shestakov.ivan/FT/dictionary.cpp
+++ shestakov.ivan/FT/dictionary.cpp
@@ -10,13 +10,6 @@ namespace shestakov
   {
     return pair.second;
   }
-  std::pair< size_t, std::string > returnSet(const std::pair< std::string, size_t > &pair)
-  {
-    std::pair< size_t, std::string > ret_pair;
-    ret_pair.first = pair.second;
-    ret_pair.second = pair.first;
-    return ret_pair;
-  }
   bool compSet(const std::pair< std::string, size_t > &first, const std::pair< std::string, size_t > &second)
   {
     return first.second < second.second;
--- shestakov.ivan/FT/dictionary.h
+++ shestakov.ivan/FT/dictionary.h
@@ -12,7 +12,6 @@ namespace shestakov
   using my_map = std::map< std::string, Dictionary >;
   bool searchWrd(const Dictionary &dictionaries, const std::string &word);
   size_t countFreq(const std::pair< std::string, size_t > &dictionary);
-  std::pair< size_t, std::string > returnSet(const std::pair< std::string, size_t > &pair);
   bool compSet(const std::pair< std::string, size_t > &first, const std::pair< std::string, size_t > &second);
   bool compWord(const std::pair<std::string, size_t> &pair, const Dictionary &dictionary);
 }
